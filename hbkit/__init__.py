1f53107171edfddb1234d55a15bb593245f252ac
Hongbo He <hhbcarl@gmail.com>
2017-08-09 21:58:41 +0800

    Support print version. fix #4

--- hbkit/__init__.py
+++ hbkit/__init__.py
@@ -3,8 +3,20 @@ from __future__ import absolute_import
 import click
 from . import core, random, short, watch
 
+__version__ = '0.4'
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo('Current version is: ' + __version__)
+    ctx.exit()
+
 
 @click.group()
+@click.option('--version', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True,
+              help='Print out current version.')
 def cli():
     core.setup()
 
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
+import hbkit
 
 
 setup(
     name='hbkit',
-    version='0.3',
+    version=hbkit.__version__,
     packages=['hbkit'],
     install_requires=['Click', 'requests'],
     entry_points={
