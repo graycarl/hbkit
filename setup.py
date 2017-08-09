d1f65fc3253b9d88b2de99cdc6b0727bd9b64506
Hongbo He <hhbcarl@gmail.com>
2017-08-09 22:40:02 +0800

    Fix get version in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+import re
+import ast
 from setuptools import setup
-import hbkit
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+
+with open('hbkit/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 
 setup(
     name='hbkit',
-    version=hbkit.__version__,
+    version=version,
     packages=['hbkit'],
     install_requires=['Click', 'requests'],
     setup_requires=['pytest-runner'],
