dc59ba35dad31d02adb41e534fa54b28085e9109
Hongbo He <hongbo.he@veeva.com>
2019-06-21 17:47:58 +0800

    Fix for click warning in python2

--- hbkit/upgrade.py
+++ hbkit/upgrade.py
@@ -1,5 +1,4 @@
-from __future__ import absolute_import, unicode_literals
-from builtins import *      # noqa
+from __future__ import absolute_import
 import click
 try:
     from pip import main as pipmain
