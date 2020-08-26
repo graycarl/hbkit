252dfadbd9653c27bf3a7daab7dd96e33a6bdd0f
Hongbo He <hhbcarl@gmail.com>
2020-08-27 00:31:52 +0800

    Do not need to implement yaml order load

--- hbkit/clash.py
+++ hbkit/clash.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
 import click
-from collections import OrderedDict
 import pkg_resources
 try:
     import yaml
@@ -38,8 +37,8 @@ def cli_build_config(template, origin, proxy_type, reverse):
             .decode('utf-8')
     else:
         template = template.read()
-    template = ordered_load(template)
-    origin = ordered_load(origin.read())
+    template = yaml.load(template)
+    origin = yaml.load(origin.read())
     proxies = origin.get('proxies') or origin.get('Proxy')
     if proxy_type:
         proxies = list(filter(lambda p: p['type'] == proxy_type, proxies))
@@ -49,34 +48,4 @@ def cli_build_config(template, origin, proxy_type, reverse):
     for group in template['proxy-groups']:
         if group['name'] in ['FAST', 'FALLBACK', 'SPECIFY']:
             group['proxies'] = [p['name'] for p in proxies]
-    click.echo(ordered_dump(template, allow_unicode=True))
-
-
-# Use OrderedDict to keep key-value order in yaml file.
-# Got solution from: https://stackoverflow.com/a/21912744
-def ordered_load(stream):
-
-    class OrderedLoader(yaml.Loader):
-        pass
-
-    def construct_mapping(loader, node):
-        loader.flatten_mapping(node)
-        return OrderedDict(loader.construct_pairs(node))
-    OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
-    return yaml.load(stream, OrderedLoader)
-
-
-def ordered_dump(data, stream=None, Dumper=None, **kwds):
-    Dumper = yaml.Dumper if Dumper is None else Dumper
-
-    class OrderedDumper(Dumper):
-        pass
-
-    def _dict_representer(dumper, data):
-        return dumper.represent_mapping(
-            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-            data.items())
-    OrderedDumper.add_representer(OrderedDict, _dict_representer)
-    return yaml.dump(data, stream, OrderedDumper, **kwds)
+    click.echo(yaml.dump(template, allow_unicode=True))
