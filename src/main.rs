c32aca660c05dd297bfe15ead1b1e261aedf489d
Hongbo He <hongbo.he@veeva.com>
2022-08-03 16:18:45 +0800

    random string

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
+use rand;
+use rand::distributions::DistString;
 use clap::Parser;
+use hbkit::random::RandomDist;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about=None)]
 struct Args {
-    /// Name of the person to greet
-    #[clap(short, long, value_parser)]
-    name: String,
-
-    /// Number of times to greet
-    #[clap(short, long, value_parser, default_value_t = 1)]
-    count: u8
+    /// Random string length
+    #[clap(short, long, value_parser, default_value_t = 12)]
+    length: u8
 }
 
 fn main() {
     let args = Args::parse();
+    let rd = RandomDist::new("naf").unwrap();
+    let mut rng = rand::thread_rng();
 
-    for _ in 0..args.count {
-        println!("Hello {}", args.name);
-    }
+    println!("Hello {}", rd.sample_string(&mut rng, args.length as usize));
 }
---
+++ src/random.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+use rand::distributions::DistString;
+
+pub struct RandomDist {
+    source: Vec<char>,
+}
+
+impl DistString for RandomDist {
+    fn append_string<R: Rng + ?Sized>(&self, rng: &mut R, string: &mut String, len: usize) {
+        let source_len = self.source.len();
+        for _ in 0..len {
+            let x: usize = rng.gen();
+            string.push(self.source[x % source_len]);
+        }
+    }
+}
+
+impl RandomDist {
+    /// Create a new RandomDist
+    /// flags:
+    /// - n: 0..9
+    /// - a: a..z
+    /// - u: A..Z
+    /// - f: !@#$%&*
+    pub fn new(flags: &str) -> Result<Self, String> {
+        let mut s = String::new();
+        for f in flags.chars() {
+            match f {
+                'n' => s.push_str("0123456789"),
+                'a' => s.push_str("abcdefghijklmnopqrstuvwxyz"),
+                'u' => s.push_str("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+                'f' => s.push_str("!@#$%&*"),
+                _ => return Err(format!("Unknown flag: '{}'", f))
+            }
+        }
+        return Ok(RandomDist {
+            source: s.chars().collect()
+        })
+    }
+}
